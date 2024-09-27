9333218fb1e79b6bb68eb3f36ff19b9c7c8b2996
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-27 08:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	  // Regular expression to validate email addresses
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  // Check if the string matches the email pattern
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
